3dd0cc6a5046550b2863366cd5a740f9b20945af
Buffon-tries <92947574+Buffon-tries@users.noreply.github.com>
2024-01-20 15:34:36 +0200

    Update login.dart

--- Project2/lib/login.dart
+++ Project2/lib/login.dart
@@ -9,8 +9,6 @@ import 'home.dart';
 class ApiConstants {
   static const String apiUrl =
       'https://zw-project-2024.000webhostapp.com/login.php';
-
-  ///'https://aspen-amusement.000webhostapp.com/login.php';
 }
 
 class User {
@@ -18,9 +16,6 @@ class User {
   int user_id;
   User({required this.username, required this.user_id});
 }
-
-String username = '';
-int user_id = 0;
 
 class LoginPage extends StatefulWidget {
   const LoginPage({Key? key}) : super(key: key);
@@ -34,214 +29,109 @@ class _LoginPageState extends State<LoginPage> {
   TextEditingController passwordController = TextEditingController();
 
   Future<void> loginUser() async {
-    print('Inside the loginUser function');
-    final response = await http.post(Uri.parse(ApiConstants.apiUrl), body: {
-      "email": emailController.text,
-      "password": passwordController.text,
-    });
-    print('response received');
-    if (response.statusCode == 200) {
-      // if successful call
-      final jsonResponse = convert.jsonDecode(response.body);
-      // create dart json object from json array// create dart json object from json array
-      print('decoding response');
-      username = jsonResponse['username'];
-      user_id = int.parse(jsonResponse['uid']);
+    try {
+      final response = await http.post(Uri.parse(ApiConstants.apiUrl), body: {
+        "email": emailController.text,
+        "password": passwordController.text,
+      });
 
-      print('username ' + username + " retreived successfully ");
-      print("user Id ${user_id}  retreived successfully ");
-      Navigator.of(context).push(MaterialPageRoute(
-          builder: (context) => DarkShowPostData(),
-          settings: RouteSettings(
-              arguments: User(username: username, user_id: user_id))));
+      if (response.statusCode == 200) {
+        final jsonResponse = convert.jsonDecode(response.body);
 
-      setState(() {});
-    } else {
-      print('faild to load email');
+        String? username = jsonResponse['username'];
+        int? user_id = int.tryParse(jsonResponse['uid'] ?? '');
+        print(user_id);
+        if (username != null && user_id != null) {
+          Navigator.of(context).push(MaterialPageRoute(
+            builder: (context) => DarkShowPostData(),
+            settings: RouteSettings(
+                arguments: User(username: username, user_id: user_id)),
+          ));
+        } else {
+          print('Username or user ID is null in the response.');
+        }
+      } else {
+        print('Failed to login');
+      }
+    } catch (e) {
+      print('Error: $e');
     }
   }
 
   @override
   Widget build(BuildContext context) {
-    final double screenWidth = MediaQuery.of(context).size.width;
     return Scaffold(
       backgroundColor: Colors.black,
       appBar: AppBar(
         backgroundColor: Colors.black,
       ),
-
       body: Container(
-        child: Stack(children: [
-          Positioned(
-              width: screenWidth,
-              height: 130,
-              child: Container(
-                padding: const EdgeInsets.only(
-                  left: 14,
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Text(
+              'Login',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 25,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 16),
+            TextField(
+              controller: emailController,
+              style: TextStyle(color: Colors.white),
+              decoration: InputDecoration(
+                hintText: 'Email',
+                hintStyle: TextStyle(color: Colors.grey),
+              ),
+            ),
+            SizedBox(height: 16),
+            TextField(
+              controller: passwordController,
+              style: TextStyle(color: Colors.white),
+              obscureText: true,
+              decoration: InputDecoration(
+                hintText: 'Password',
+                hintStyle: TextStyle(color: Colors.grey),
+              ),
+            ),
+            SizedBox(height: 30),
+            ElevatedButton(
+              onPressed: () {
+                loginUser();
+              },
+              style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.all<Color>(Colors.grey),
+              ),
+              child: Text(
+                'Login',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 20,
                 ),
-                color: Colors.black,
-                child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text('Welcome',
-                          style: GoogleFonts.comfortaa(
-                            textStyle: const TextStyle(
-                                fontSize: 34,
-                                color: Colors.white,
-                                fontWeight: FontWeight.bold),
-                          )),
-                      const SizedBox(height: 8),
-                      Text('Please enter your email and password to',
-                          style: GoogleFonts.alef(
-                              textStyle: const TextStyle(
-                                  color: Colors.white,
-                                  fontSize: 18,
-                                  fontWeight: FontWeight.w500))),
-                      Text('login',
-                          style: GoogleFonts.alef(
-                              textStyle: const TextStyle(
-                                  color: Colors.white,
-                                  fontSize: 18,
-                                  fontWeight: FontWeight.w500))),
-                    ]),
-              )),
-          Positioned(
-              width: screenWidth,
-              top: 190,
-              height: 800,
-              child: Container(
-                decoration: const BoxDecoration(
-                    gradient: LinearGradient(
-                        begin: Alignment.topCenter,
-                        end: Alignment.bottomCenter,
-                        colors: [Color(0xffffffff), Colors.white])),
-              )),
-          Positioned(
-              width: screenWidth * 0.9,
-              height: 438,
-              top: 120,
-              left: screenWidth * 0.05,
-              child: Material(
-                elevation: 24,
-                shadowColor: Color(0xff1b1b1b),
-                borderRadius: BorderRadius.circular(10),
-                child: Container(
-                    padding: const EdgeInsets.symmetric(
-                        horizontal: 16, vertical: 17),
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(10),
-                      color: const Color(0xffffffff),
-                    ),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text('Login',
-                            style: GoogleFonts.alef(
-                              textStyle: const TextStyle(
-                                  color: Colors.black,
-                                  fontSize: 25,
-                                  fontWeight: FontWeight.bold),
-                            )),
-                        const SizedBox(height: 16),
-                        Container(
-                            margin: const EdgeInsets.only(left: 4),
-                            width: 40,
-                            height: 4,
-                            color: Colors.black),
-                        const SizedBox(height: 26),
-                        MyTextField(
-                            hintText: 'email',
-                            ctr: emailController,
-                            hasPreIcon: false,
-                            fontSize: 16),
-                        const SizedBox(height: 16),
-                        MyTextField(
-                          hintText: 'Password',
-                          hasPreIcon: true,
-                          ctr: passwordController,
-                          fontSize: 16,
-                          preIcon: const Icon(Icons.remove_red_eye, size: 20),
-                        ),
-                        const SizedBox(height: 26),
-                        Container(
-                            width: 135,
-                            margin: const EdgeInsets.only(left: 180),
-                            child: Text('Forgot Password?',
-                                style: GoogleFonts.alef(
-                                    textStyle: const TextStyle(
-                                        color: Color(0xff4b4141),
-                                        letterSpacing: 0.2,
-                                        fontSize: 12)))),
-                        Container(
-                          margin: const EdgeInsets.only(left: 5, top: 20),
-                          width: screenWidth * 0.78,
-                          height: 45,
-                          child: TextButton(
-                            onPressed: () {
-                              print('clicked on login');
-                              print('invoking the loginUser function ');
-                              loginUser();
-                            },
-                            style: ButtonStyle(
-                              // Change the background color
-                              backgroundColor: MaterialStateProperty.all<Color>(
-                                  const Color(0xffbfbfbc)),
-
-                              // Change the text color
-                              foregroundColor: MaterialStateProperty.all<Color>(
-                                  Colors.white),
-
-                              // Add a border around the button
-                              shape: MaterialStateProperty.all<
-                                      RoundedRectangleBorder>(
-                                  RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(18.0),
-                              )),
-                            ),
-                            child: Text('Login',
-                                style: GoogleFonts.alef(
-                                    textStyle: const TextStyle(
-                                        fontWeight: FontWeight.bold,
-                                        color: Colors.black,
-                                        fontSize: 20))),
-                          ),
-                        ),
-                        const SizedBox(height: 30),
-                        Row(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            crossAxisAlignment: CrossAxisAlignment.end,
-                            children: [
-                              Text("Don't have an account? ",
-                                  style: GoogleFonts.alef(
-                                      textStyle: const TextStyle(
-                                          //fontWeight: FontWeight.bold,
-                                          color: Color(0xff645f5f),
-                                          fontSize: 14))),
-                              const SizedBox(width: 4),
-                              GestureDetector(
-                                onTap: () {
-                                  Navigator.of(context).push(MaterialPageRoute(
-                                    builder: (context) => SignupPage(),
-                                  ));
-                                },
-                                child: Text('SIGN UP',
-                                    style: GoogleFonts.alef(
-                                        textStyle: const TextStyle(
-                                            fontWeight: FontWeight.bold,
-                                            color: Colors.black,
-                                            decoration:
-                                                TextDecoration.underline,
-                                            decorationColor: Colors.black,
-                                            fontSize: 16))),
-                              )
-                            ])
-                      ],
-                    )),
-              ))
-        ]),
+              ),
+            ),
+            SizedBox(height: 16),
+            GestureDetector(
+              onTap: () {
+                Navigator.of(context).push(MaterialPageRoute(
+                  builder: (context) => SignupPage(),
+                ));
+              },
+              child: Text(
+                'Don\'t have an account? Sign up',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 14,
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
-
-      //Container(),
     );
   }
 }
